69476e271d9f0f71f28b8b6eca09e1ded1a75e93
hyeon <dandan9509@gmail.com>
2019-09-24 12:52:39 +0900

    Add: 슬랙에서만 요청응답 검수확인

--- app.js
+++ app.js
@@ -2,28 +2,42 @@ const
   express = require('express'),
   app = express(),
   shell = require('shelljs'),
-  path = require('path');
+  path = require('path'),
+  signVerification = require('./signVerification');
 
-app.get('/control/:proejct/:type', (request, response) => {
-  let project = request.params.project;
-  let type = request.params.type;
-  let proejctDir = path.join(__dirname,'..', project , 'Gulp');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  switch(type) {
-    case 'start' :
-      // // Session start
-      shell.exec(`screen -dmSL ${proejct} gulp --cwd ${proejctDir}`);
-      break;
-    case 'stop' :
-      // Session drop
-      shell.exec(`screen -X -S ${project} quit`);
-      break;
-    case 'test' :
-      console.log('TEST Success!');
-      break;
-    default :
-      break;
+app.post('/control/:project/:type', (request, response) => {
+  try{
+    const nodeApi = (request, response) => {
+      let project = request.params.project;
+      let type = request.params.type;
+      let projectDir = path.join(__dirname,'..', project , 'Gulp');
+
+      switch(type) {
+        case 'start' :
+          // // Session start
+          shell.exec(`screen -dmSL ${project} gulp --cwd ${projectDir}`);
+          response.status(201).json({'text': `Start \`${project}\``});
+          response.end();
+          break;
+        case 'stop' :
+          // Session drop
+          shell.exec(`screen -X -S ${project} quit`);
+          response.status(201).json({'text': `Stop \`${project}\``});
+          response.end();
+          break;
+        default :
+          break;
+      }
+    };
+
+    signVerification(response, request, nodeApi);
+  } catch ( err ) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.end();
   }
 });
 
-app.listen(3000);
+app.listen(8080, '127.0.0.1');
